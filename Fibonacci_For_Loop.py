35106c61b8017b78683651d080183f7d07b69306
Mukul61929 <150412726+Mukul61929@users.noreply.github.com>
2023-11-10 10:59:45 +0530

    Update Fibonacci_For_Loop.py

--- Fibonacci_For_Loop.py
+++ Fibonacci_For_Loop.py
@@ -1,19 +1,23 @@
-n = 10
-
+n=10
 Num1 = 0
 Num2 = 1
 
 if(n == 2):
     print(f"The Fibonacci series for",n,"is:")
     print(Num1)
+    print("-----------")
     print(Num2)
+    print("-----------")
 else:
     print(f"The Fibonacci series for",n,"is :")
     print(Num1)
+    print("-----------")
     print(Num2)
-
-    for i in range(n-2):
+    print("-----------")
+    
+    for index in range(n-2):
         next_num = Num1+Num2
         print(next_num)
+        print("-----------")
         Num1 = Num2
         Num2 = next_num
