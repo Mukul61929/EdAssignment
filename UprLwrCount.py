f79907f09b76d584cbbb62028ed95fbf2f7d3b37
Mukul61929 <150412726+Mukul61929@users.noreply.github.com>
2023-11-22 23:14:01 +0530

    Create UprLwrCount.py

---
+++ UprLwrCount.py
@@ -0,0 +1,16 @@
+def my_str_check(s):
+    lower=0
+    upper=0
+    for i in s:
+	    if i.isupper():
+		        upper+=1
+	    elif i.islower():
+		        lower+=1
+    print(f"The given string is {s}")     
+    print("---------------------------------------------")
+    print("The number of lowercase characters is:",lower)
+    print("---------------------------------------------")
+    print("The number of uppercase characters is:",upper)
+
+my_str=("The quick Brow Fox")
+my_str_check(my_str)
