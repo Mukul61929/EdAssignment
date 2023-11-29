4b104fce3292dcc6a4f4937ac1a914ae8270719e
Mukul61929 <150412726+Mukul61929@users.noreply.github.com>
2023-11-29 09:46:06 +0530

    Create MapFN.py

---
+++ MapFN.py
@@ -0,0 +1,6 @@
+# Program to make the input number three times as output
+list1=[1,2,3,4,5,6,7]
+result= list(map(lambda num : num * 3, list1))
+print(f"Given input list is: {list1}")
+print("--------------------------------------------------------------------------------")
+print(f"The output result with three times of input numbers is: {result}")
