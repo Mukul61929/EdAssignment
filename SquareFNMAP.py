d0f72e4d96cde63cdb3c0746cd4934f0108f0f12
Mukul61929 <150412726+Mukul61929@users.noreply.github.com>
2023-11-29 09:58:59 +0530

    Create SquareFNMAP.py

---
+++ SquareFNMAP.py
@@ -0,0 +1,5 @@
+list1=[4,5,2,9]
+result= list(map(lambda num : num * num, list1))
+print(f"Given input list is: {list1}")
+print("--------------------------------------------------------------------------------")
+print(f"The output result with square of input list is : {result}")
